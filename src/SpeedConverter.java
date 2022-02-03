e016f74e0b0ad57009a30c775de89550e879a745
umairbin.yousaf <umairbin.yousaf@stud.uni-goettingen.de>
2022-02-03 21:18:09 +0100

    done some changes in output

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -11,7 +11,7 @@ public class SpeedConverter {
 
     public static void printConversion(double kilometerPerHour){
         if (kilometerPerHour < 0){
-            System.out.println("Invalid Value");
+            System.out.println("Invalid Value occurs");
         }else {
             long milesPerHour = toMilesPerHour(kilometerPerHour);
             System.out.println(kilometerPerHour + " km/h = " + milesPerHour + " mi/h");
